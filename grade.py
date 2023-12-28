d7df762e61a500cfc5a28425a3d531a435137511
Roy Evert Andrej Dolonius <124092984+Gigglinglizard@users.noreply.github.com>
2023-12-28 13:12:47 +0100

    Added comments for clarity

--- grade.py
+++ grade.py
@@ -9,9 +9,11 @@ load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
 BASE_URL = 'https://canvas.kth.se/api/v1'
-COURSE_ID = '41678'  
+COURSE_ID = '41678' # EN2720 Ethical Hacking
+
 
 def get(url):
+    """Fetch data from the provided URL."""
     response = requests.get(
         url,
         headers={'Authorization': f'Bearer {API_KEY}'}
@@ -25,12 +27,23 @@ def get(url):
         data.extend(response.json())
     return data
 
+
 def get_all_assignments():
+    """
+    Fetch all assignments for the course. 
+    Specifically fetch 'capturing flag' assignments.
+    """
     url = f'{BASE_URL}/courses/{COURSE_ID}/assignments'
     assignments = get(url)
     return [assignment for assignment in assignments if 'capturing flag' in assignment['name'].lower()]
 
+
 def calculate_grade(assignments):
+    """
+    Calculates the grade for the course.
+    Earned points are calculated by fetching score data for each assignment.
+    The grade is calculated as the percentage of earned points to total points.
+    """
     total_points = 0
     earned_points = 0
 
@@ -42,6 +55,7 @@ def calculate_grade(assignments):
             earned_points += submission['score']
 
     return (earned_points / total_points) * 100 if total_points else 0
+
 
 assignments = get_all_assignments()
 grade = calculate_grade(assignments)
